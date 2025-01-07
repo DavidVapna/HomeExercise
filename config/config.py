f0a8c7f0849f88abe76c71f5d3d919290d329acf
David Vapna <88938066+DavidVapna@users.noreply.github.com>
2025-01-07 16:58:49 +0200

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,6 +1,6 @@
 import configparser
 import os
-
+#config.ini file required, private data kept there.
 config = configparser.ConfigParser()
 config.read(os.path.join(os.path.dirname(__file__), 'config.ini'))
 
